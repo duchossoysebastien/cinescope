e477f3148ec79ae6351b317deeeacf159a6f4d57
duchossoysébsatien <sebastienduchossoy@yahoo.fr>
2026-02-16 13:36:06 +0100

    route du template film valide

--- src/Controller/PlatformeController.php
+++ src/Controller/PlatformeController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PlatformeRepository;
 
 final class PlatformeController extends AbstractController
 {
     #[Route('/platforme', name: 'app_platforme')]
-    public function index(): Response
+    public function index(PlatformeRepository $platformeRepository): Response
     {
+        $platformes = $platformeRepository->findAll();
         return $this->render('platforme/index.html.twig', [
-            'controller_name' => 'PlatformeController',
+            'platformes' => $platformes,
         ]);
     }
 }
